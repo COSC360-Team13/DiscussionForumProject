5e4cd7f67d9af98b4dc1751fc5d8de11db97827e
Pawan Bilkhu <pawanbilkhu@Pawans-MacBook-Pro.local>
2021-04-08 23:02:08 -0700

    Renamed several files for better clarification. New user page now validates all fields and displays if a given username is available or not.

--- html/userExistsCheck.php
+++ html/userExistsCheck.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
 <?php
-include 'config.php'
+include 'config.php';
+
 if(isset($_POST['username'])){
     $username = $_POST['username'];
-    $statment = $pdo->prepare("SELECT COUNT(*) AS countUser FROM user WHERE username=:username");
+    $statment = $pdo->prepare("SELECT COUNT(*) FROM users WHERE username=:username");
     $statment->bindValue(':username', $username, PDO::PARAM_STR);
     $statment->execute();
-    $row = $statment->fetchColumn();
+    $column = $statment->fetchColumn();
 
     $response = "<p style='color: green;'>Available</p>";
-    if($row > 0)
+    if($column > 0)
         $response = "<p style='color: red;'>Unavailable</p>";
     echo $response;
     exit();
