792cf6f481bb508bcab1966f04be01fcc271b584
Pawan Bilkhu <pawanbilkhu@Pawans-MacBook-Pro.local>
2021-04-09 14:19:08 -0700

    Started implementation for search functionality

--- html/navBar.php
+++ html/navBar.php
@@ -5,7 +5,7 @@
     <nav class="navbar">
         <div class="logo"><a href="mainPage.php"><img src="" alt="HomepageLogo"></a></div>
         <div class="searchbar">
-            <input type="text" placeholder="Search for Bears..." \></div>
+            <input type="text" placeholder="Search for Bears..." \><button class="searchButton" onclick="search()">Search</button></div>
         <div class="account"><button><img src="" alt="Account"></button></div>
     </nav>
 </header>
