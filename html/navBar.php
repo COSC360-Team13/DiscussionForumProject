5d260660704ec9e4e7e1ec3b3a7efe67cc9cbdb2
Pawan Bilkhu <pawanbilkhu@Pawans-MacBook-Pro.local>
2021-04-11 14:51:48 -0700

    Fixed navBar positioning. It is now a fixed element on the page. Renamed SearchResultsPage to searchResultsPage. Adding the word Page to the end of every webpage that a user will see on the site. Added a js script to retrieve searchbar text.

--- html/navBar.php
+++ html/navBar.php
@@ -1,11 +1,15 @@
 <head>
 <link rel="stylesheet" href="../css/navBar.css">
+<script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+<!-- In the event that jquery doesnt load through the web. -->
+<script type="text/javascript" src="../scripts/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="../scripts/searchBar.js"></script>
 </head>
 <header>
-    <nav class="navbar">
+    <div class="navbar">
         <div class="logo"><a href="mainPage.php"><img src="" alt="HomepageLogo"></a></div>
         <div class="searchbar">
-            <input type="text" placeholder="Search for Bears..." \><button class="searchButton" onclick="search()">Search</button></div>
+            <input type="text" id="searchTerm" placeholder="Search for Bears..." \><button class="searchButton" onclick="search()">Search</button></div>
         <div class="account"><button><img src="" alt="Account"></button></div>
-    </nav>
+    </div>
 </header>
--- html/searchResultsPage.php
+++ html/searchResultsPage.php
@@ -2,16 +2,17 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/mainPage.css">
+    <link rel="stylesheet" href="../css/searchResults.css">
     <title>Search Results</title>
 </head>
 <?php include 'navBar.php'; ?>
 <body>
+<div class="main">
 <div id="breadcrumbs"></div>
 <div class="columns">
     <div class="grid-container">
         <div class="grid-header">
-            <h2>Categories</h2>
+            <h2>About</h2>
         </div>
         <div class="grid-entry">
             <div class="grid-item">1</div>
@@ -19,12 +20,26 @@
     </div>
     <div class="grid-container">
         <div class="grid-header center">
-            <span></span>
+            <button class="button top">Top</button>
+            <button class="button new">New</button>
             <h2>Subtopics</h2>
-            <button class="newTopic">Create subtopic</button>
+            <button class="button create">Create</button>
         </div>
         <div class="grid-entry">
             <div class="grid-item">Thing</div>
+            <?php include 'config.php';
+            $sql = "SELECT DISTINCT category FROM subtopic";
+            $results = $pdo->query($sql);
+            while($row = $results->fetch())
+            {
+                // TODO: update link to redirect to correct page
+                echo "<a href=\"#catgeory=".$row["category"]."\">";
+                echo "<div class=\"grid-item\">".$row["category"];
+                echo "</div>";
+                echo "</a>";
+            }
+            $pdo = null;
+            $results = null;
         </div>
     </div>
     <div class="grid-container">
@@ -35,6 +50,7 @@
             <a href="#"><div class="grid-item">3</div></a>
         </div>
     </div>
+</div>
 </div>
 
 </body>
