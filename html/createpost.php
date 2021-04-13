82c5f9c2217718ffa706e59c26874a709167ade2
Pierre Frigon <pierref97@gmail.com>
2021-04-13 00:01:40 -0700

    Update createpost.php

--- html/createpost.php
+++ html/createpost.php
@@ -10,30 +10,49 @@
       <?php
        include 'config.php';
 
-      if($_SERVER["REQUEST_METHOD"] === "POST"){
-        $posttitle = $_POST["posttitle"];
+       $msg = "";
 
+      if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $posttitle = $_POST["posttitle"];
         $postcontent = $_POST["postcontent"];
+        $username = $_SESSION["username"];
+        $image = $_POST["upload"];
 
         if(isset($postcontent)){
-          $statement = $pdo->prepare("INSERT INTO users (title, , username, email, password) VALUES (?, ?, ?, ?, ?)");
+          $statement = $pdo->prepare("INSERT INTO users (ptitle,username,content,date) VALUES (?, ?, ?, ?, ?)");
+          $statement = execute([$posttitle, $postcontent, $username, now()]);
         }
 
+        if(isset($image)){
+          $filename = $_FILES["image"]["name"];
+          $tempname = $_FILES["image"]["tmp_name"];
+          $folder = "image/".$filename;
 
+          $sql = "INSERT INTO image (filename) VALUES ('$filename')";
 
-        $statement = $pdo->prepare("INSERT INTO POST (, title, username, email, password) VALUES (?, ?, ?, ?, ?)");
-        $statement->execute([$firstname, $lastname, $username, $email, $password]);
+          mysqli_query($pdo, $sql);
+
+          if(move_uploaded_file($tempname, $folder)) {
+            $msg = "Image uploaded successfully";
+          }
+          else{
+            $msg = "Failed to upload image";
+          }
+          $result = mysqli_query($pdo, "SELECT * FROM image");
+
+        }
       }
       ?>
+
       <img src="Images/Group 16.png" alt="Italian Trulli">
       <h1>Create Post</h1>
-      <form action="createpost.php" method="get" target="_blank">
+      <form action="createpost.php" method="post" target="_blank">
         <input type = "text" id = "posttitle" name = "posttitle" placeholder="Title...">
         <input type = "text" id = "postcontent" name = "postcontent" placeholder="Content...">
       </form>
       <form action="upload.php" method="post" enctype="multipart/form-data">
   Post Image:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
+  <input type="file" name="image" id="image">
+  <input type="submit" value="Upload Image" name="upload">
 </form>
   </body>
